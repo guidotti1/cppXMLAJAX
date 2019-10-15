ee1c1bfc928b006e59c03a22363963630c54c078
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-15 13:41:57 -0400

    Add files via upload

---
+++ figuringOutXML.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#define XML_USE_STL
+#include "xmlParser.h"
+
+using namespace std;
+
+
+int main (int argc, char ** argv) {
+
+    string gameFile = "testGame.xml";
+
+    // this opens and parses the correct XML file:
+    XMLNode xMainNode=XMLNode::openFileHelper(gameFile.c_str(),"map");
+    int i = 0;
+    // get the first room node
+    XMLNode roomNode=xMainNode.getChildNode(i++);
+
+
+    do {
+    int noNodes = nChildNode(roomNode);
+
+    cout << "NoNodes " << noNodes << endl;
+
+    roomNode=xMainNode.getChildNode(i++);
+    } while (!roomNode.isEmpty());
+
+    cout << endl;
+    return 0;
+}
